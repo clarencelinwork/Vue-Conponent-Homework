9a0080c568f5a664d2bd906cef504e274519d08d
clarencelinwork <clarencelinwork@gmail.com>
2025-09-02 09:42:59 +0800

    [feature] update itemList and pass value

--- src/App.vue
+++ src/App.vue
@@ -2,6 +2,51 @@
 import ItemListView from '@/views/ItemList.vue'
 import CartView from '@/views/Cart.vue'
 import NotifyToastView from '@/views/NotifyToast.vue'
+
+import { ref } from 'vue'
+const itemList = ref([
+  {
+    id: 1,
+    name: '耳罩式藍牙耳機',
+    description: '舒適配戴，支援降噪技術',
+    price: 2490,
+    imgUrl:
+      'https://images.unsplash.com/photo-1546435770-a3e426bf472b?q=80&amp;w=2065&amp;auto=format&amp;fit=crop&amp;ixlib=rb-4.1.0&amp;ixid=M3wxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D',
+  },
+  {
+    id: 2,
+    name: '耳罩式彩虹耳機',
+    description: '舒適配戴，支援降噪技術',
+    price: 1380,
+    imgUrl:
+      'https://images.unsplash.com/photo-1546435770-a3e426bf472b?q=80&amp;w=2065&amp;auto=format&amp;fit=crop&amp;ixlib=rb-4.1.0&amp;ixid=M3wxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D',
+  },
+  {
+    id: 3,
+    name: '時尚藍牙耳機',
+    description: '舒適配戴，支援降噪技術',
+    price: 7990,
+    imgUrl:
+      'https://images.unsplash.com/photo-1546435770-a3e426bf472b?q=80&amp;w=2065&amp;auto=format&amp;fit=crop&amp;ixlib=rb-4.1.0&amp;ixid=M3wxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D',
+  },
+  {
+    id: 4,
+    name: '機械式鍵盤',
+    description: '紅軸機械鍵盤，打字手感極佳',
+    price: 1890,
+    imgUrl:
+      'https://images.unsplash.com/photo-1546435770-a3e426bf472b?q=80&amp;w=2065&amp;auto=format&amp;fit=crop&amp;ixlib=rb-4.1.0&amp;ixid=M3wxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D',
+  },
+  {
+    id: 5,
+    name: '無線滑鼠',
+    description: '靜音按鍵設計，長效電池',
+    price: 890,
+    imgUrl:
+      'https://images.unsplash.com/photo-1546435770-a3e426bf472b?q=80&amp;w=2065&amp;auto=format&amp;fit=crop&amp;ixlib=rb-4.1.0&amp;ixid=M3wxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D',
+  },
+])
+
 </script>
 
 <template>
@@ -9,7 +54,7 @@ import NotifyToastView from '@/views/NotifyToast.vue'
     <div class="row">
       <!-- 商品列表區 -->
       <div class="col-md-8">
-        <ItemListView />
+        <ItemListView :itemList="itemList"/>
       </div>
 
       <!-- 購物車區 -->
